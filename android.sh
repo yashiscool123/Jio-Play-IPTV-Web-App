8000c19fb544f47f1341cd0df639568c9a272215
Mobassar@4u <92091277+Mobassar4u@users.noreply.github.com>
2023-01-07 10:14:52 +0530

    Update android.sh

--- android.sh
+++ android.sh
@@ -14,7 +14,7 @@ mkdir .termux/boot
 mv /data/data/com.termux/files/usr/share/apache2/default-site/htdocs/auto_boot.sh ./.termux/boot/
 apachectl start
 termux-wake-lock
-echo 'JIO TV' | figlet
-echo 'PHP \n SERVER' | figlet
+echo 'Jio TV' | figlet
+echo 'PHP \n Server' | figlet
 echo '\033[42m Server Installed Successfully.\n Go to the below Address \n to login and continue setup \033[0m'
 echo '\033[32m '`ip route get 1.2.3.4 | awk '{print $9}'`':8080\n\033[0m'
