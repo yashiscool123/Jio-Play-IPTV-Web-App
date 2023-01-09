18ae2f0623583f68eb6ddb0087f10a59d3af7c6a
Mobassar@4u <92091277+Mobassar4u@users.noreply.github.com>
2023-01-09 11:22:17 +0530

    Update play.php

--- play.php
+++ play.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-<title><?php echo $_REQUEST["c"]; ?> | JioTV LIVE</title>
+<title><?php echo $_REQUEST["c"]; ?> | Jio TV Live</title>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1"/>
 <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
